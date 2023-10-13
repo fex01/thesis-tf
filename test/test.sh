1d2ab9ea046324396f53b653402832e554926907
fex <git@fexmail.de>
2023-10-13 09:53:23 +0200

    EF tf plan fails without provider config

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 
 for i in $(seq 99); do
-    curl -X POST http://localhost:8080/view/Thesis%20Pipelines/job/Pipeline%20IaC%20and%20IaCSec/build?delay=0sec --data verbosity=high --user Matt2212:118ac65a7b184f21395807e323fd5030aa
+    curl -X POST http://localhost:8080/job/thesis/build?delay=0sec --data verbosity=high --user admin:e17a935058ef4a6ca1cd07aec9a0de45
     echo "execution $i"
     sleep 40
 done

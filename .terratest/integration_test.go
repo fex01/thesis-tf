8e3a0370cb26710798aedf7e01231281b2a5e300
fex <git@fexmail.de>
2023-10-15 06:55:26 +0000

    added terratest files

---
+++ .terratest/integration_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraform(t *testing.T) {
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+
+	dbPwd := "test1234"
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// website::tag::1:: Set the path to the Terraform code that will be tested.
+		TerraformDir: "../",
+
+		Vars: map[string]interface{}{
+			"db_pwd": dbPwd,
+		},
+	})
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
+	output := terraform.Output(t, terraformOptions, "endpoint")
+	assert.Contains(t, ".eu-west-3.rds.amazonaws.com", output)
+}
